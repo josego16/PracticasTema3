6cfd94a550dfa556bf075d28d44ab27df141e875
josego16 <jgomlin@g.educaand.es>
2023-12-06 18:40:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practicastema3/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.practicastema3
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.practicastema3.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+}
---
+++ app/src/main/java/com/example/practicastema3/SecondFragment.kt
@@ -0,0 +1,26 @@
+package com.example.practicastema3
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.practicastema3.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+}
